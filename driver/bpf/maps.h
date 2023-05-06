3fce92c28f48cc783751e214d5d2f25202731fbf
yaofighting <siyao@zju.edu.cn>
2023-05-06 18:32:57 +0800

    adjust to __bpf_section(maps).
    Signed-off-by: yaofighting <siyao@zju.edu.cn>

--- driver/bpf/maps.h
+++ driver/bpf/maps.h
@@ -114,42 +114,42 @@ struct bpf_map_def __bpf_section("maps") tcp_handshake_map = {
 	.max_entries = 65535,
 };
 
-struct bpf_map_def SEC("maps") tcp_handshake_buffer = {
+struct bpf_map_def __bpf_section("maps") tcp_handshake_buffer = {
 	.type = BPF_MAP_TYPE_PERCPU_HASH,
 	.key_size = sizeof(u64),
 	.value_size = sizeof(struct tcp_handshake_buffer_elem),  
 	.max_entries = MAX_BUFFER_LEN,
 };
 
-struct bpf_map_def SEC("maps") tcp_datainfo_map = {
+struct bpf_map_def __bpf_section("maps") tcp_datainfo_map = {
 	.type = BPF_MAP_TYPE_HASH,
 	.key_size = sizeof(struct tcp_tuple),
 	.value_size = sizeof(struct tcp_datainfo_last),  
 	.max_entries = 65535,
 };
 
-struct bpf_map_def SEC("maps") tcp_datainfo_buffer = {
+struct bpf_map_def __bpf_section("maps") tcp_datainfo_buffer = {
 	.type = BPF_MAP_TYPE_PERCPU_HASH,
 	.key_size = sizeof(u64),
 	.value_size = sizeof(struct tcp_datainfo),  
 	.max_entries = MAX_BUFFER_LEN,
 };
 
-struct bpf_map_def SEC("maps") tcp_buffer_pointer = {
+struct bpf_map_def __bpf_section("maps") tcp_buffer_pointer = {
 	.type = BPF_MAP_TYPE_PERCPU_HASH,
 	.key_size = sizeof(u64),
 	.value_size = sizeof(u64), //this type is u64 because all array elements are aligned to 8 bytes.
 	.max_entries = 16,
 };
 
-struct bpf_map_def SEC("maps") focus_network_interface = {
+struct bpf_map_def __bpf_section("maps") focus_network_interface = {
 	.type = BPF_MAP_TYPE_HASH,
 	.key_size = sizeof(u32),
 	.value_size = sizeof(u64),  
 	.max_entries = 1024,
 };
 
-struct bpf_map_def SEC("maps") tcp_rawdata_buffer = {
+struct bpf_map_def __bpf_section("maps") tcp_rawdata_buffer = {
 	.type = BPF_MAP_TYPE_PERCPU_HASH,
 	.key_size = sizeof(u64),
 	.value_size = sizeof(struct tcp_raw_data),  
